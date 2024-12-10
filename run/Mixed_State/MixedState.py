08f8b7dd4fa8b277521e9dba70753ce9dcfa10e9
Binho <bin262@gmail.com>
2024-12-10 10:16:18 +0900

    add new code for VEW

--- run/Mixed_State/MixedState.py
+++ run/Mixed_State/MixedState.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.insert(0, '../')
+sys.path.insert(0, '../../')
 from qutip import qeye 
 import matplotlib.pyplot as plt 
 from qutip import basis, tensor, ket2dm, sigmax, sigmaz
--- run/ZX/ZX_plot.py
+++ run/ZX/ZX_plot.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.insert(0, '../')
+sys.path.insert(0, '../../')
 import numpy as np
 import matplotlib.pyplot as plt
 from qutip import basis, tensor, sigmax, sigmaz
